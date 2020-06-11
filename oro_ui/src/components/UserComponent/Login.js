32607adb088ec39416c6bc3f3f83eaca5c592c64
akash <akash59tiwari@gmail.com>
2020-06-11 12:06:29 +0530

    Initial commit for Login Page

---
+++ oro_ui/src/components/UserComponent/Login.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Login extends Component{
+
+    render(){
+        return(
+          <>
+          <h2>This is login page</h2>
+          </>
+        );
+    }
+
+}
+
+export default Login;
